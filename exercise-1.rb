57986ec8634b9331a306cc12a648cf067e99ddd9
Beedeeboom <stephanie.duffieux@gmail.com>
2020-04-06 11:00:03 +1000

    added exercise 1

--- exercise-1.rb
+++ exercise-1.rb
@@ -1,4 +1,8 @@
 # Create an array of strings (with five elements). Make a loop from this array, and inside that loop concatenate the string with the string “balloon”, and print this to output.
 
+array_strings = ["one", "two", "three", "four", "five"]
 
+5.times do
+    p array_strings.join("" + " balloon ")
+end
 
