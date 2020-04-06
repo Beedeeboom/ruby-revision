d65fab9343e41167b878a27c67b19ea2895624fc
Beedeeboom <stephanie.duffieux@gmail.com>
2020-04-06 10:41:32 +1000

    added new branch

---
+++ exercise-1.rb
@@ -0,0 +1,4 @@
+# Create an array of strings (with five elements). Make a loop from this array, and inside that loop concatenate the string with the string “balloon”, and print this to output.
+
+
+
